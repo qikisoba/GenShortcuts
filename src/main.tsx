ea962f34b0cdccc974b45cc2d8a4d9dfb627334a
urtblw <dgoskiy@gmail.com>
2024-05-11 22:53:35 +0700

    1.0.5

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { store } from './store'
 import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/GenShortcuts">
+  <BrowserRouter basename="/GenShortcuts/">
     <Provider store={store}>
       <App />
     </Provider>
